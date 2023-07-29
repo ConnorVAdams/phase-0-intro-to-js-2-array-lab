601220dc1e72b95d2eaef21b2e2e7b787747d043
Connor Adams <connorvadams@gmail.com>
2023-07-29 16:18:07 -0600

    Submits Arrays Lab assignment.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+let newCats = []
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+};
+
+function appendCat(name) {
+    newCats = [...cats, "Broom"];
+    return newCats;
+};
+
+function prependCat(name) {
+    newCats = ["Arnold", ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    newCats = cats.slice(0, 2);
+    return newCats;
+}
+
+function removeFirstCat() {
+    newCats = cats.slice(1, 3);
+    return newCats;
+}
